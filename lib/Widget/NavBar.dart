ec53abb2ef1c0c30b2e9fd129b3db589a858482e
Lasitha Harshana <lasitha6646@gmail.com>
2024-01-18 10:46:25 +0530

    navbar email.add

--- lib/Widget/NavBar.dart
+++ lib/Widget/NavBar.dart
@@ -1,10 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../src/features/authentication/controllers/authentication_repository.dart';
 
 class NavBar extends StatelessWidget {
-  const NavBar({super.key});
-
+  NavBar({super.key});
+  final user = FirebaseAuth.instance.currentUser!;
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -14,7 +15,7 @@ class NavBar extends StatelessWidget {
         children: [
           UserAccountsDrawerHeader(
             accountName: const Text('Oflutter.com'),
-            accountEmail: const Text('example@gmail.com'),
+            accountEmail: Text(user.email!),
             currentAccountPicture: CircleAvatar(
               child: ClipOval(
                 child: Image.network(
