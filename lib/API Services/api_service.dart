73afc37251bc0dc7b8058f5ce0be8a5f117df381
Chathurya Prasad <121708803+CPrasa@users.noreply.github.com>
2024-01-18 13:55:18 +0530

    merge

--- lib/API Services/api_service.dart
+++ lib/API Services/api_service.dart
@@ -11,11 +11,7 @@ class ApiService {
     final String apiUrl = 'https://livescore6.p.rapidapi.com/matches/v2/$live';
 
     final Map<String, String> headers = {
-<<<<<<< HEAD
       //'X-RapidAPI-Key': '889b3cc290msh3a6002ff66b137dp1c409bjsn0d6ab11fc890',
-=======
-      // 'X-RapidAPI-Key': '889b3cc290msh3a6002ff66b137dp1c409bjsn0d6ab11fc890',
->>>>>>> 1dfa16fc31b35feadebd199e1ba139a931f308a8
       //'X-RapidAPI-Key': 'cec61bf002mshae792f4044c9c8bp1fb688jsnfe5dfb44071c',
       'X-RapidAPI-Key': '4b22548360msh6e7148078d97ae8p180705jsn11d44b0880f0',
       'X-RapidAPI-Host': 'livescore6.p.rapidapi.com',
--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,9 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'GoCric',
-      theme: ThemeData(primarySwatch: Colors.blue, brightness: Brightness.dark),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: const WelcomeScreen(),
     );
   }
